782dc380039473a97d96fc858244a6a70e6f0d9e
Craig MacFarlane <camacfarl@gmail.com>
2021-07-30 09:30:11 -0700

    Fix acls for public objects

--- terraform/core/storage.tf
+++ terraform/core/storage.tf
@@ -23,10 +23,22 @@ resource "google_storage_bucket" "fmltc-blobs" {
   depends_on = [google_project_service.gcp_services]
 }
 
-resource "google_storage_bucket_acl" "fmltc-public-acl" {
+#resource "google_storage_bucket_acl" "fmltc-public-acl" {
+# bucket = google_storage_bucket.fmltc.name
+# predefined_acl = "publicread"
+# depends_on = [google_project_service.gcp_services]
+#
+
+#resource "google_storage_default_object_acl" "fmltc-public-object_acl" {
+  #bucket = google_storage_bucket.fmltc.name
+  #role_entity = [ "READER:allUsers" ]
+  #depends_on = [google_project_service.gcp_services]
+#}
+
+resource "google_storage_default_object_access_control" "public_rule" {
   bucket = google_storage_bucket.fmltc.name
-  predefined_acl = "publicread"
-  depends_on = [google_project_service.gcp_services]
+  role   = "READER"
+  entity = "allUsers"
 }
 
 
@@ -52,15 +64,19 @@ resource "google_storage_bucket" "fmltc-gae-source" {
 }
 
 resource "google_storage_bucket_object" "styles" {
-  name   = "static/css/styles.css"
-  source = "${path.root}/../../server/static/css/styles.css"
-  bucket = google_storage_bucket.fmltc.name
+  name         = "static/css/styles.css"
+  source       = "${path.root}/../../server/static/css/styles.css"
+  content_type = "text/css"
+  bucket       = google_storage_bucket.fmltc.name
+  depends_on   = [google_storage_default_object_access_control.public_rule]
 }
 
 resource "google_storage_bucket_object" "closure_js" {
-  name   = "compiled/js/fmltc.js"
-  source = "${path.root}/../../compiled/js/fmltc.js"
-  bucket = google_storage_bucket.fmltc.name
+  name         = "compiled/js/fmltc.js"
+  source       = "${path.root}/../../compiled/js/fmltc.js"
+  content_type = "application/css"
+  bucket       = google_storage_bucket.fmltc.name
+  depends_on   = [google_storage_default_object_access_control.public_rule]
 }
 
 
