4dd17f7793f87f840ddbece2fd19a0634b75bedf
Craig MacFarlane <camacfarl@gmail.com>
2021-09-30 09:02:34 -0700

    Add remote backend

---
+++ terraform/dev/backend.tf
@@ -0,0 +1,14 @@
+terraform {
+  backend "gcs" {
+    #
+    # Terraform is broken w.r.t. variables in a backend block.
+    # So we must resort to hardcoded values for the bucket and
+    # credentials.
+    #
+    # The bucket should be created via gsutil before running terraform
+    # for the first time.
+    #
+    bucket = "ftc-ml-firstinspires-dev-tf-state"
+    credentials = "../../server/key.json"
+  }
+}
