b928cee87e05ba262d1061a3ac2cde9745b5d13e
Craig MacFarlane <camacfarl@gmail.com>
2021-10-06 20:33:29 -0700

    Add the prod directory

---
+++ terraform/prod/main.tf
@@ -0,0 +1,10 @@
+module "dev" {
+  source = "../core"
+
+  project_id        = var.project_id
+  region            = var.region
+  app_engine_region = var.app_engine_region
+  project_url       = var.project_url
+  zone              = var.zone
+  environment       = var.environment
+}
