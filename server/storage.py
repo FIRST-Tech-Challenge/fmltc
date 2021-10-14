10e3d4368abe8457c092ad58a7da9b0430f3133a
Uday Vidyadharan <55924756+uvidyadharan@users.noreply.github.com>
2021-10-14 19:02:40 -0400

    Update storage.py

--- server/storage.py
+++ server/storage.py
@@ -93,10 +93,8 @@ def retrieve_team_uuid(program, team_number):
                 'preferences': {},
                 'last_video_uuid': '',
                 'videos_uploaded_today': 0,
-                'datasets_downloaded_today': 0,
                 'datasets_created_today': 0,
-                'models_downloaded_today': 0,
-                
+                'datasets_downloaded_today': 0,
             })
         else:
             team_entity = team_entities[0]
