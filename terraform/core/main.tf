6207dc18c693842666c68a00a6628348905d1881
Liz Looney <lizlooney@google.com>
2022-09-01 13:50:41 -0700

    Added png to static for WithSupportFromGoogleCloud.png

--- terraform/core/main.tf
+++ terraform/core/main.tf
@@ -204,6 +204,32 @@ resource "google_app_engine_standard_app_version" "fmltc-app-v1" {
     auth_fail_action = "AUTH_FAIL_ACTION_REDIRECT"
     login            = "LOGIN_OPTIONAL"
     security_level   = "SECURE_OPTIONAL"
+    url_regex = "/(.*\\.ico)"
+    static_files {
+      path = "static/\\1"
+      mime_type = "image/x-icon"
+      upload_path_regex = "static/(.*\\.ico)"
+      expiration = "0s"
+    }
+  }
+
+  handlers {
+    auth_fail_action = "AUTH_FAIL_ACTION_REDIRECT"
+    login            = "LOGIN_OPTIONAL"
+    security_level   = "SECURE_OPTIONAL"
+    url_regex = "/(.*\\.png)"
+    static_files {
+      path = "static/\\1"
+      mime_type = "image/png"
+      upload_path_regex = "static/(.*\\.png)"
+      expiration = "0s"
+    }
+  }
+
+  handlers {
+    auth_fail_action = "AUTH_FAIL_ACTION_REDIRECT"
+    login            = "LOGIN_OPTIONAL"
+    security_level   = "SECURE_OPTIONAL"
     url_regex = "/(.*\\.txt)"
     static_files {
       path = "static/\\1"
@@ -283,4 +309,3 @@ resource "google_project_iam_binding" "tpu_role" {
     "serviceAccount:${data.ml_config.cfg.service_account}"
   ]
 }
-
